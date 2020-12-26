895dab0ef82bfbb48ef7d5f0eefee87da411b0ce
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2020-12-26 20:36:18 +0530

    final

--- app.py
+++ app.py
@@ -70,29 +70,109 @@ st.write(user_data)
 # MODEL
 rf  = RandomForestClassifier()
 rf.fit(x_train, y_train)
-st.subheader('Accuracy: ')
-st.write(str(accuracy_score(y_test, rf.predict(x_test))*100)+'%')
 user_result = rf.predict(user_data)
-st.subheader('Your Report: ')
-output=''
-if user_result[0]==0:
-  output = 'You are healthy'
-else:
-  output = 'You are not healthy'
-st.title(output)
-
-
 
 
 
 # VISUALISATIONS
-fig_pregnancies = plt.figure(figsize = (10,6))
-ax1 = sns.distplot(df['Pregnancies'] , bins = 100)
-ax2 = sns.distplot(user_data['pregnancies'])
-st.pyplot(fig_pregnancies)
+st.title('Visualised Patient Report')
 
+
+
+# COLOR FUNCTION
+if user_result[0]==0:
+  color = 'blue'
+else:
+  color = 'red'
+
+
+# Age vs Pregnancies
+st.header('Pregnancy count Graph (Others vs Yours)')
+fig_preg = plt.figure()
+ax1 = sns.scatterplot(x = 'Age', y = 'Pregnancies', data = df, hue = 'Outcome', palette = 'Greens')
+ax2 = sns.scatterplot(x = user_data['age'], y = user_data['pregnancies'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,20,2))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_preg)
+
+
+
+# Age vs Glucose
+st.header('Glucose Value Graph (Others vs Yours)')
 fig_glucose = plt.figure()
-ax1 = sns.scatterplot(x = 'Outcome', y = 'Glucose', data = df , hue = 'Outcome')
-ax2 = sns.scatterplot(y = user_data['glucose'], x = user_result[0], color='red', s = 100)
+ax3 = sns.scatterplot(x = 'Age', y = 'Glucose', data = df, hue = 'Outcome' , palette='magma')
+ax4 = sns.scatterplot(x = user_data['age'], y = user_data['glucose'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,220,10))
+plt.title('0 - Healthy & 1 - Unhealthy')
 st.pyplot(fig_glucose)
 
+
+
+# Age vs Bp
+st.header('Blood Pressure Value Graph (Others vs Yours)')
+fig_bp = plt.figure()
+ax5 = sns.scatterplot(x = 'Age', y = 'BloodPressure', data = df, hue = 'Outcome', palette='Reds')
+ax6 = sns.scatterplot(x = user_data['age'], y = user_data['bp'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,130,10))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_bp)
+
+
+# Age vs St
+st.header('Skin Thickness Value Graph (Others vs Yours)')
+fig_st = plt.figure()
+ax7 = sns.scatterplot(x = 'Age', y = 'SkinThickness', data = df, hue = 'Outcome', palette='Blues')
+ax8 = sns.scatterplot(x = user_data['age'], y = user_data['skinthickness'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,110,10))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_st)
+
+
+# Age vs Insulin
+st.header('Insulin Value Graph (Others vs Yours)')
+fig_i = plt.figure()
+ax9 = sns.scatterplot(x = 'Age', y = 'Insulin', data = df, hue = 'Outcome', palette='rocket')
+ax10 = sns.scatterplot(x = user_data['age'], y = user_data['insulin'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,900,50))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_i)
+
+
+# Age vs BMI
+st.header('BMI Value Graph (Others vs Yours)')
+fig_bmi = plt.figure()
+ax11 = sns.scatterplot(x = 'Age', y = 'BMI', data = df, hue = 'Outcome', palette='rainbow')
+ax12 = sns.scatterplot(x = user_data['age'], y = user_data['bmi'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,70,5))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_bmi)
+
+
+# Age vs Dpf
+st.header('DPF Value Graph (Others vs Yours)')
+fig_dpf = plt.figure()
+ax13 = sns.scatterplot(x = 'Age', y = 'DiabetesPedigreeFunction', data = df, hue = 'Outcome', palette='YlOrBr')
+ax14 = sns.scatterplot(x = user_data['age'], y = user_data['dpf'], s = 150, color = color)
+plt.xticks(np.arange(10,100,5))
+plt.yticks(np.arange(0,3,0.2))
+plt.title('0 - Healthy & 1 - Unhealthy')
+st.pyplot(fig_dpf)
+
+
+
+# OUTPUT
+st.subheader('Your Report: ')
+output=''
+if user_result[0]==0:
+  output = 'You are not Diabetic'
+else:
+  output = 'You are Diabetic'
+st.title(output)
+st.subheader('Accuracy: ')
+st.write(str(accuracy_score(y_test, rf.predict(x_test))*100)+'%')
