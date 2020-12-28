da25e85f6eb1a3677b05a8fc428de9d8f03eb3f1
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2020-12-29 01:03:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,14 @@
-# mkdir -p ~/.streamlit/
-
-# echo "\
-# [general]\n\
-# email = \"akmadan17@gmail.com\"\n\
-# " > ~/.streamlit/credentials.toml
-
-# echo "\
-# [server]\n\
-# headless = true\n\
-# enableCORS=false\n\
-# port = $PORT\n\
-# " > ~/.streamlit/config.toml
 mkdir -p ~/.streamlit/
-echo “
-[server]
-headless = true
-enableCORS=false
-port = $PORT
-“ > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"akmadan17@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
